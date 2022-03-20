bfc98e6577ec60061d54913ca6532a6fe18cb558
nessa <vanessa242pierre@gmail.com>
2022-03-20 16:16:35 -0400

    Vanessa: Added Test Set 10, fixed a bug and fixed style issues

--- a3/Tests/testRandomizedSearchCVUnit.py
+++ a3/Tests/testRandomizedSearchCVUnit.py
@@ -1,13 +1,10 @@
-import numpy as np
 from functools import reduce
 import itertools
+import numpy as np
+import scipy
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
-import sys
-import os
-
-
 
 SEP = '.' * 150
 LINE = '=' * 150
@@ -25,61 +22,71 @@ def testRandomizedSearchCV(case, desc, params, expt, iters, repetition):
     print(LINE)
 
     X, y = load_iris(return_X_y=True)
-
     rf = RandomForestClassifier()
-
 
     print("Input:")
 
     for i in range(len(params)):
-        print("dict" + str (i + 1) + ": " + str(params[i]))
+        print("\t dict" + str(i + 1) + ": " + str(params[i]))
 
-    rand = RandomizedSearchCV(rf, params, cv=5, scoring='accuracy', n_iter=iters, random_state=1, with_replacement=repetition)
+    rand = RandomizedSearchCV(
+        rf, params, cv=5, scoring='accuracy',
+            n_iter=iters, random_state=1) # , without_replacement=!repetition
     rand.fit(X, y)
 
     print(SEP)
-
     print("Output:")
 
     # Gets all the unique dictionary keys
-    param_keys = [x for x in list(rand.cv_results_.keys()) if x.startswith("param_")]
+    param_keys = [x for x in list(
+        rand.cv_results_.keys()) if x.startswith("param_")]
 
     # Build results as tuples
-    results = list(zip(*[list((j if j != np.ma.core.MaskedConstant else None) for j in rand.cv_results_[param_keys[i]]) for i in range(len(param_keys))]))
+    results = list(zip(*[list((
+        j if j != np.ma.core.MaskedConstant else None)
+            for j in rand.cv_results_[
+                param_keys[i]]) for i in range(len(param_keys))]))
 
-    print(str (tuple(param_keys)) + ": \n" + str (results))
-
+    print("\t" + str(tuple(param_keys)) + ": \n \t" + str(results))
     print(SEP)
-
     print("Test Results:")
 
+    chosen = [0] * len(params)
+    masterlist = rand.cv_results_['param_n_estimators']
+    for i in masterlist:
+        for j in range(len(params)):
+            if i in params[j]['n_estimators']:
+                chosen[j] += 1
+                break
 
     # Test 1: Make sure selection has uniform distribution
-    chosen = []
-    for i in range(len(params)):
-        chosen.append(len(list(filter(lambda x:(x in params[i]['n_estimators']),rand.cv_results_['param_n_estimators']))))
-
     print("     Test 1: ")
     print("             dict#  times choosen  actual %  expected %")
     print("             ------------------------------------------")
     for i in range(len(chosen)):
-        p = "{:>7}  {:>11}  {:>9}".format(str (chosen[i]),  str(round((chosen[i] / len(results)) * 100, 2)), str (round(float (expt[i]), 2)))
-        print("             dict" + str (i + 1) + " | " + p)
+        p = "{:>7}  {:>11}  {:>9}".format(str(chosen[i]),  str(round((
+            chosen[i] / len(results)) * 100, 2)), str(
+                round(float (expt[i]), 2)))
+        print("             dict" + str(i + 1) + " | " + p)
 
     # Test 2: Make sure contains a valid amount of combos
-    combinations = sum([reduce(lambda x, y: x*y, i) for i in [list(len(i[j]) for j in i) for i in params]])
-
-    try :
-        assert (repetition and len(results) == iters) or (not repetition and len(results) == min(combinations, iters))
-        print("PASS Test 2: The amount of resulting combinations is valid.")
-    except :
-        print("FAIL Test 2: The result should have a size of, " + str (iters if repetition else min(combinations, iters)) + ", not " + str(len(results)))
-
-    # Test 3: Make sure each tuple belongs to a dictionary
     combos = []
     for k in range(len(params)):
-        combos = combos + list(itertools.product(*(params[k][i] for i in params[k].keys())))
+        combos = combos + list(itertools.product(*(
+        params[k][i] for i in params[k].keys() if type(params[k][i]) == list)))
 
+    combinations = len(combos)
+
+    try :
+        assert (repetition and len(results) == iters) or (
+            not repetition and len(results) == min(combinations, iters))
+        print("PASS Test 2: The amount of resulting combinations is valid.")
+    except :
+        print("FAIL Test 2: The result should have a size of, " + str(
+            iters if repetition else min(
+                combinations, iters)) + ", not " + str(len(results)))
+
+    # Test 3: Make sure each tuple belongs to a dictionary
     mismatch = []
 
     for i in range(len(results)):
@@ -95,7 +102,8 @@ def testRandomizedSearchCV(case, desc, params, expt, iters, repetition):
         assert len(mismatch) == 0
         print("PASS Test 3: The resulting combinations are valid.")
     except :
-        print("FAIL Test 3: The following are invalid combinations, " + str (mismatch))
+        print("FAIL Test 3: The following are invalid combinations, " + str(
+            mismatch))
 
     # Test 4: If repetition is off, make sure each tuple is unique
     if not repetition:
@@ -110,7 +118,6 @@ def testRandomizedSearchCV(case, desc, params, expt, iters, repetition):
 
 
 def testAll(cases):
-    os.system('cls' if os.name == 'nt' else 'clear')
     for case in cases.keys():
         testCase = cases[case]
         desc = testCase['desc']
@@ -123,35 +130,40 @@ def testAll(cases):
 
 
 testCases = {
-    'Test 1: no repetition and equal distribution I' : {
+    'Test Set 1: Repetition off and Equal Distribution, 1 Param Dictionaries' : {
         'desc': 'repetition = False; iterations = 10',
         'args': [10, False,
-                 {'n_estimators': [10, 20], 'min_samples_leaf': [1, 2, 3, 4, 5]}],
+                 {'n_estimators': [10, 20],
+                    'min_samples_leaf': [1, 2, 3, 4, 5]}],
         'expt': [100]
         },
-    'Test 2: no repetition and equal distribution II' : {
+    'Test Set 2: Repetition off and Equal Distribution, 2 Param Dictionaries' : {
         'desc': 'repetition = False; iterations = 16',
         'args': [16, False,
-                 {'n_estimators': [10, 20, 30], 'min_samples_leaf': [5, 6], 'bootstrap': [True, False]},
+                 {'n_estimators': [10, 20, 30], 'min_samples_leaf': [5, 6],
+                    'bootstrap': [True, False]},
                  {'n_estimators': [40, 50], 'min_samples_leaf': [1, 2, 3, 4]}],
         'expt': [50, 50]
         },
-    'Test 3: no repetition and equal distribution III' : {
+    'Test Set 3: Repetition off and Equal Distribution, 3 Param Dictionaries' : {
         'desc': 'repetition = False; iterations = 30',
         'args': [30, False,
-                 {'n_estimators': [10, 20, 30], 'min_samples_leaf': [1, 2, 3, 4, 5]},
-                 {'n_estimators': [40, 50, 60], 'min_samples_leaf': [4, 5, 6, 7, 8, 9, 10]},
-                 {'n_estimators': [50, 60, 70, 80, 90, 100, 110], 'bootstrap': [False, True]}],
+                 {'n_estimators': [10, 20, 30],
+                    'min_samples_leaf': [1, 2, 3, 4, 5]},
+                 {'n_estimators': [40, 50, 60],
+                    'min_samples_leaf': [4, 5, 6, 7, 8, 9, 10]},
+                 {'n_estimators': [50, 60, 70, 80, 90, 100, 110],
+                    'bootstrap': [False, True]}],
         'expt': [33.33, 33.33, 33.33]
         },
-    'Test 4: no repetition and inequal distribution I' : {
+    'Test Set 4: Repetition off and Unequal Distribution, 2 Param Dictionaries' : {
         'desc': 'repetition = False; iterations = 7',
         'args': [7, False,
                  {'n_estimators': [10, 20], 'min_samples_leaf': [1, 2]},
                  {'n_estimators': [30], 'min_samples_leaf': [4, 5, 6]}],
         'expt': [57.14, 42.86]
         },
-    'Test 5: no repetition and inequal distribution II' : {
+    'Test Set 5: Repetition on and Unequal Distribution, 4 Param Dictionaries' : {
         'desc': 'repetition = False; iterations = 14',
         'args': [14, False,
                  {'n_estimators': [10, 20, 30], 'min_samples_leaf': [1, 2]},
@@ -160,37 +172,60 @@ testCases = {
                  {'n_estimators': [70]}],
         'expt': [42.86, 21.43, 28.57, 7.14]
         },
-    'Test 6: no repetition and inequal distribution III' : {
+    'Test Set 6: Repetition on and Unequal Distribution, 3 Param Dictionaries' : {
         'desc': 'repetition = False; iterations = 20',
         'args': [25, False,
-                 {'n_estimators': [10, 20, 30, 40], 'min_samples_leaf': [1, 2, 3]},
+                 {'n_estimators': [10, 20, 30, 40],
+                    'min_samples_leaf': [1, 2, 3]},
                  {'n_estimators': [50], 'min_samples_leaf': [4, 5, 6, 7, 8, 9]},
-                 {'n_estimators': [50, 60, 70, 80, 90, 100, 110], 'min_samples_leaf': [1, 2], 'bootstrap': [False, True]}],
+                 {'n_estimators': [50, 60, 70, 80, 90, 100, 110],
+                    'min_samples_leaf': [1, 2], 'bootstrap': [False, True]}],
         'expt': [38, 24.0, 38]
         },
-    'Test 7: repetition I' : {
+    'Test Set 7: Repetition on, 1 Param Dictionaries' : {
         'desc': 'repetition = True; iterations = 50',
         'args': [50, True,
                  {'n_estimators': [10, 20], 'min_samples_leaf': [1, 2, 3]}],
         'expt': [100]
         },
-    'Test 8: repetition II' : {
+    'Test Set 8: Repetition on, 2 Param Dictionaries' : {
         'desc': 'repetition = True; iterations = 50',
         'args': [50, True,
-                 {'n_estimators': [10, 20, 30, 40], 'min_samples_leaf': [1, 2, 3]},
-                 {'n_estimators': [50, 60, 70, 80, 90, 100, 110], 'bootstrap': [False, True]}],
+                 {'n_estimators': [10, 20, 30, 40],
+                    'min_samples_leaf': [1, 2, 3]},
+                 {'n_estimators': [50, 60, 70, 80, 90, 100, 110],
+                    'bootstrap': [False, True]}],
         'expt': [50, 50]
         },
-    'Test 9: repetition III' : {
+    'Test Set 9: Repetition on, 4 Param Dictionaries' : {
         'desc': 'repetition = True; iterations = 50;',
         'args': [50, True,
                  {'n_estimators': [130]},
-                 {'n_estimators': [10, 20, 30, 40], 'min_samples_leaf': [1, 2, 3, 4]},
-                 {'n_estimators': [80, 90, 100, 110, 120], 'bootstrap': [False, True]},
-                 {'n_estimators': [50, 60, 70, 80], 'min_samples_leaf': [1, 2, 3, 4, 5, 6, 7, 8], 'max_features': ['auto', None], 'bootstrap': [True, False]}],
+                 {'n_estimators': [10, 20, 30, 40],
+                    'min_samples_leaf': [1, 2, 3, 4]},
+                 {'n_estimators': [80, 90, 100, 110, 120],
+                    'bootstrap': [False, True]},
+                 {'n_estimators': [50, 60, 70, 80],
+                    'min_samples_leaf': [1, 2, 3, 4, 5, 6, 7, 8],
+                    'max_features': ['auto', None],
+                    'bootstrap': [True, False]}],
+        'expt': [25, 25, 25, 25]
+        },
+    'Test Set 10: Repetition with non-list Parameters' : {
+        'desc': 'repetition = True; iterations = 50;',
+        'args': [50, True,
+                 {'n_estimators': [130]},
+                 {'n_estimators': [10, 20, 30, 40],
+                    'min_samples_leaf': scipy.stats.uniform(
+                        loc=0.25, scale=0.2)},
+                 {'n_estimators': [80, 90, 100, 110, 120],
+                    'bootstrap': [False, True]},
+                 {'n_estimators': [50, 60, 70, 80],
+                    'min_samples_leaf': [1, 2, 3, 4, 5, 6, 7, 8],
+                    'max_features': ['auto', None],
+                    'bootstrap': [True, False]}],
         'expt': [25, 25, 25, 25]
         }
 }
-
 
 testAll(testCases)
