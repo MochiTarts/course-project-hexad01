883d361e53b269b59d350c851fbd3e60820214dd
Autumn Jiang <autumn@Autumn-Polaris.local>
2022-04-03 16:41:33 -0400

    Kara: Updated Acceptance test script

--- a4/Tests/testBisectingKMeansAcceptance.py
+++ a4/Tests/testBisectingKMeansAcceptance.py
@@ -6,7 +6,7 @@ from sklearn.datasets import make_blobs
 
 indent = ' ' * 4
 SEP = '_' * 100 + '\n'
-LINE = '=' * 150
+LINE = '=' * 100
 
 
 def executeTest(case, desc, case_num, useBKM=False, fit=True, predict=True,
@@ -14,18 +14,19 @@ def executeTest(case, desc, case_num, useBKM=False, fit=True, predict=True,
                 predicted_clusters=20, n_clusters=20, n_init=10, max_iter=300,
                 fit_random_state=0, predict_random_state=1):
 
-    print(LINE)
-    print(case, ":", desc)
+    print("\n" * 2 + LINE)
+    print(case)
+    print('\n' + indent + desc)
     print(LINE)
 
     mode = "Bisecting K-Means" if useBKM else "K-Means"
     if verbose:
         print(SEP)
-        print(mode, "Acceptance Testing")
+        print("Starting", mode, "Acceptance Testing:")
     KM = BisectingKMeans if useBKM else KMeans
 
     # Create datasets
-    verbose and print(SEP + "[1] Creating Dataset 1\n")
+    verbose and print(SEP + "[1/5] Creating Dataset 1\n")
     X1, y1 = make_blobs(
         n_samples=n_samples, n_features=n_features,
         centers=fitted_clusters, cluster_std=0.5,
@@ -33,15 +34,14 @@ def executeTest(case, desc, case_num, useBKM=False, fit=True, predict=True,
     )
     #verbose and print("X1 =", X1)
 
-    verbose and print(SEP + "[2] Creating Dataset 2\n")
+    verbose and print(SEP + "[2/5] Creating Dataset 2\n")
     X2, y2 = make_blobs(
         n_samples=n_samples, n_features=n_features,
         centers=predicted_clusters, cluster_std=0.5,
         shuffle=True, random_state=predict_random_state
     )
-    #verbose and print("X2 =", X2)
 
-    verbose and print(SEP + "[3] Instantializing:\n")
+    verbose and print(SEP + "[3/5] Instantializing:\n")
     verbose and print(indent, "Setting:",
                       "n_clusters=" + str(n_clusters) + ",",
                       "init='random',",
@@ -55,19 +55,21 @@ def executeTest(case, desc, case_num, useBKM=False, fit=True, predict=True,
         tol=1e-04, random_state=fit_random_state
     )
 
-    verbose and print(SEP + "[4] Running\n")
+    verbose and print(SEP + "[4/5] Running\n")
     y_km1 = km.fit_predict(X1)
     y_km2 = km.predict(X2)
     colours = np.random.rand(np.amax(km.labels_) + 1, 3)
     matplotlib_axes_logger.setLevel('ERROR')
-    predict and producePredictResultPlot(km, X2, y_km2, colours, mode,
-                                         verbose, case_num)
-    fit and produceFitResultPlot(km, X1, y_km1, colours, mode,
-                                 verbose, case_num)
+    
+    predict and producePredictResultPlot(
+        km, X2, y_km2, colours, mode, verbose, case_num)
+    fit and produceFitResultPlot(
+        km, X1, y_km1, colours, mode,verbose, case_num)
+    
     verbose and print(
         SEP +
-        "[5] Finished. Please check your file manager " +
-        "for the resulting plots.")
+        "[5/5] Finished. Please check your file manager " +
+        "for the resulting plots.\n" + LINE)
 
 
 def producePredictResultPlot(km, X2, y_km2, colours, mode, verbose, case_num):
@@ -89,10 +91,12 @@ def producePredictResultPlot(km, X2, y_km2, colours, mode, verbose, case_num):
             label=': Centroid'
         )
     plt.legend(scatterpoints=1, bbox_to_anchor=(1, 1))
-    plt.title(mode + " Results After predict() Invocation")
+    plt.title("Test " + str(
+        case_num) + ":" + mode + " Results After predict() Invocation")
     plt.axis("square")
     plt.grid()
-    plt.savefig(mode + " Predict Results " + str(case_num) + ".png")
+    plt.savefig("Test " + str(
+        case_num) + ' ' + mode + " Predict.png")
 
 
 def produceFitResultPlot(km, X1, y_km1, colours, mode, verbose, case_num):
@@ -113,17 +117,19 @@ def produceFitResultPlot(km, X1, y_km1, colours, mode, verbose, case_num):
             c=np.array(colours[i]), edgecolor='black',
             label='centroids'
         )
-    plt.title(mode + " Results After fit() Invocation")
+    plt.title("Test " + str(
+        case_num) + ":" + mode + " Results After fit() Invocation")
     plt.axis("square")
     plt.grid()
-    plt.savefig(mode + " Fit Results " + str(case_num) + ".png")
+    plt.savefig("Test " + str(
+        case_num) + ' ' + mode + " Fit.png")
 
 
 testCases = {
-    'Test1 : Small number of samples and features, large number of clusters.': {
+    'Test 0 : Small number of samples and features, large number of clusters.': {
         'desc': 'n_samples=200, n_features=2, fitted_clusters=20, ' + \
-                'predicted_clusters=20, n_clusters=20, n_init=10, ' + \
-                'max_iter=300, fit_random_state=0, predict_random_state=1',
+                'predicted_clusters=20, n_clusters=20, n_init=10,\n' + \
+                indent + 'max_iter=300, fit_random_state=0, predict_random_state=1',
         'args': {
             'n_samples': 200,
             'n_features': 2,
@@ -136,7 +142,7 @@ testCases = {
             'predict_random_state': 1
         }
     },
-    'Test2 : Fewer generated clusters than n_clusters.': {
+    'Test 1 : Fewer generated clusters than n_clusters.': {
         'desc': 'n_samples=2000, fitted_clusters=20, ' + \
                 'predicted_clusters=20, n_clusters=30',
         'args': {
@@ -167,12 +173,13 @@ def testAll(cases, useBKM=False, verbose=True):
 
 
 def main():
+    print(LINE)
     print("This script is configured to compare" +
           "plotted results between K-Means and Bisecting K-Means. " +
-          "Note: if n_features is > 2 for any tests, only the first two " +
-          "will be plotted, which may lead to some unintuitive " +
-          "visualizations.")
-    prompt = "Please select one of the following:" + \
+          "\n[Note]\n- If n_features > 2, only the first two " +
+          "will be plotted which may yield unintuitive visualizations.")
+    print(LINE)
+    prompt = "Please select an estimator:" + \
              "\n[1] K-Means\n[2] Bisecting K-Means\n"
     selection = input(prompt)
     useBKM = (selection == "2")
