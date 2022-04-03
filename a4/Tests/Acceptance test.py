83551a7a5d51be8091be132a89d05f90538676df
Autumn Jiang <autumn@Autumn-Polaris.local>
2022-04-02 20:02:54 -0400

    Kara: Updated Acceptance test script

--- a4/Tests/Acceptance test.py
+++ a4/Tests/Acceptance test.py
@@ -4,33 +4,54 @@ import numpy as np
 from sklearn.cluster import BisectingKMeans, KMeans
 from sklearn.datasets import make_blobs
 
+indent = ' ' * 4
+SEP = '_' * 100 + '\n'
 
-def main(useBKM=True, fit=True, predict=True):
+
+def executeTest(useBKM=True, fit=True, predict=True, verbose=True):
+    if verbose:
+        print(SEP)
+        print(("Bisecting K-Means" if useBKM else "Kmeans"), "Acceptance Testing")
+    KM = BisectingKMeans if useBKM else KMeans
+    
     # Create datasets
+    verbose and print(SEP + "[1] Creating Dataset 1\n")
     X1, y1 = make_blobs(
         n_samples=200, n_features=2,
         centers=20, cluster_std=0.5,
         shuffle=True, random_state=0
     )
+    verbose and print("X1 =", X1)
+    
+    verbose and print(SEP + "[2] Creating Dataset 2\n")
     X2, y2 = make_blobs(
         n_samples=200, n_features=2,
         centers=20, cluster_std=0.5,
         shuffle=True, random_state=2
     )
-    KM = BisectingKMeans if useBKM else KMeans
+    verbose and print("X2 =", X2)
+    
+    verbose and print(SEP + "[3] Instantializing:\n")
+    verbose and print(indent, "Setting:", "n_clusters=10,", "init='random',", 
+        "n_init=10,", "max_iter=300,", "tol=1e-04,", "random_state=0")
     km = KM(
         n_clusters=10, init='random',
         n_init=10, max_iter=300, 
         tol=1e-04, random_state=0
     )
+    
+    verbose and print(SEP + "[4] Running\n")
     y_km1 = km.fit_predict(X1); y_km2 = km.predict(X2)
     colours = np.random.rand(np.amax(y_km2) + 1, 3)
     matplotlib_axes_logger.setLevel('ERROR')
-    predict and producePredictResultPlot(km, X2, y_km2, colours)
-    fit and produceFitResultPlot(km, X1, y_km1, colours)
+    predict and producePredictResultPlot(km, X2, y_km2, colours, verbose)
+    fit and produceFitResultPlot(km, X1, y_km1, colours, verbose)
+    verbose and print(
+        SEP + "[5] Finished. Please check your file manager for the resulting plots.")
 
 
-def producePredictResultPlot(km, X2, y_km2, colours):
+def producePredictResultPlot(km, X2, y_km2, colours, verbose):
+    verbose and print(indent, "Plotting Predict Results...")
     plt.figure(1)
     for i in np.unique(y_km2):
         plt.scatter(
@@ -48,12 +69,14 @@ def producePredictResultPlot(km, X2, y_km2, colours):
             label=': Centroid'
         )
     plt.legend(scatterpoints=1)
+    plt.title("Results After predict() Invocation")
     plt.axis("square")
     plt.grid()
-    plt.savefig('Predict Results.png')
+    plt.savefig('AT Predict Results.png')
 
 
-def produceFitResultPlot(km, X1, y_km1, colours):
+def produceFitResultPlot(km, X1, y_km1, colours, verbose):
+    verbose and print(indent, "Plotting Fit Results...")
     plt.figure(2)
     for i in np.unique(y_km1):
         plt.scatter(
@@ -70,7 +93,9 @@ def produceFitResultPlot(km, X1, y_km1, colours):
             c=np.array(colours[i]), edgecolor='black',
             label='centroids'
         )
+    plt.title("Results After fit() Invocation")
     plt.axis("square")
     plt.grid()
-    plt.savefig('Fit Results.png')
+    plt.savefig('AT Fit Results.png')
 
+executeTest()
